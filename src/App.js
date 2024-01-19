0172bb44c2e2d9147e36b910636fca209605defa
Almudena Rivera <riveraferalmudena@gmail.com>
2024-01-19 11:15:36 +0100

    added promo banner

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
 import './App.css';
-import LandingPage from './pages/home/bannerhome';
+import LandingPage from './pages/home/bannerHome';
 import Busqueda from './components/busqueda/busqueda';
 import ImageCarousel from './components/carrusel/carrusel';
 import datosLocales from '../src/lotocare.json';
 import Cabecera from './components/navbar/Cabecera'
 import Footer from './components/navbar/Footer'
+import PromoBanner from './pages/home/bannerPromo';
 
 function App() {
   return (
@@ -14,6 +15,7 @@ function App() {
       <LandingPage />
      
       <ImageCarousel data={datosLocales} /> Pasa los datos a ImageCarousel */
+      <PromoBanner />
       <Footer />
     </div>
   );
---
+++ src/pages/home/bannerPromo.jsx
@@ -0,0 +1,24 @@
+// src/components/PromoBanner.js
+import React from 'react';
+// import bannerImage from '../../image/brand/modelo-producto.jpg';
+import './promo.css'; 
+
+const PromoBanner = () => {
+  return (
+    <div className="promo-banner">
+      <div className="banner-content">
+        <div className="banner-left">
+          <span className="big-font-number">20%</span>
+        </div>
+        <div className="banner-middle">
+          <p>¡Nuevas ofertas en tus productos favoritos!</p>
+        </div>
+        <div className="banner-right">
+          <span className="double-arrow">➤➤</span>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default PromoBanner;
